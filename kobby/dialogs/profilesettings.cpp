9129fcff3bd266250f18404a6a6419fb54fa6def
Gregory Haynes <greg@greghaynes.net>
2009-01-28 18:16:13 -0800

    Added nick and color choser to profile settings widget.

--- kobby/dialogs/profilesettings.cpp
+++ kobby/dialogs/profilesettings.cpp
@@ -1,10 +1,15 @@
 #include "profilesettings.h"
 
+#include "ui_profilesettingswidget.h"
+
 namespace Kobby
 {
 
 ProfileSettings::ProfileSettings( QWidget *parent )
+    : QWidget( parent )
+    , ui( new Ui::ProfileSettingsWidget )
 {
+    ui->setupUi( this );
 }
 
 }
--- kobby/dialogs/profilesettings.h
+++ kobby/dialogs/profilesettings.h
@@ -1,5 +1,10 @@
 #include <QWidget>
 
+namespace Ui
+{
+    class ProfileSettingsWidget;
+}
+
 namespace Kobby
 {
 
@@ -9,6 +14,9 @@ class ProfileSettings
 
     public:
         ProfileSettings( QWidget *parent = 0 );
+    
+    private:
+        Ui::ProfileSettingsWidget *ui;
     
 };
 
