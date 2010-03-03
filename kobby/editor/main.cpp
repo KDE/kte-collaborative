c329d4a14ae2cf5f7807ed0d4524a3c76f2e2c22
Gregory Haynes <greg@greghaynes.net>
2010-03-03 13:33:34 -0800

    Update about kobby copyright year.

--- kobby/editor/main.cpp
+++ kobby/editor/main.cpp
@@ -35,7 +35,7 @@ int main( int argc, char **argv )
         ki18n( "Kobby" ), "1.0 RC 1",
         ki18n( "Collaborative text editing environment." ),
         KAboutData::License_GPL,
-        ki18n( "Copyright (c) 2008-2009 Gregory Haynes" ),
+        ki18n( "Copyright (c) 2008-2010 Gregory Haynes" ),
         KLocalizedString(),
         "http://kobby.greghaynes.net", "kobby@groups.google.com" );
     aboutData.addAuthor( ki18n("Gregory Haynes"), ki18n( "Creator, Maintainer" ), "greg@greghaynes.net" );
