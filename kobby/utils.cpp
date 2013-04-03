f4268cbb12880ec037cc49a8600948de648cd48d
Sven Brauch <svenbrauch@googlemail.com>
2013-04-03 02:20:34 +0200

    move iterlookuphelper to a seperate file

---
+++ kobby/utils.cpp
@@ -0,0 +1,81 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2013 Sven Brauch <svenbrauch@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+ */
+
+#include "utils.h"
+
+#include <QStringList>
+
+IterLookupHelper::IterLookupHelper(QString lookupPath, QInfinity::Browser* browser)
+        : QObject()
+        , m_browser(browser)
+        , m_currentIter(*m_browser)
+{
+    // remove starting slash
+    if ( lookupPath.startsWith('/') ) {
+        lookupPath = lookupPath.mid(1);
+    }
+    m_remainingComponents << lookupPath.split('/').toVector();
+    kDebug() << "finding iter for" << m_remainingComponents;
+};
+
+void IterLookupHelper::finished_cb(InfcNodeRequest* request, void* user_data)
+{
+    static_cast<IterLookupHelper*>(user_data)->directoryExplored();
+}
+
+void IterLookupHelper::explore(QInfinity::BrowserIter directory)
+{
+    if ( ! directory.isExplored() ) {
+        kDebug() << "exploring iter";
+        InfcExploreRequest* request = directory.explore();
+        m_currentIter = directory;
+        g_signal_connect_after(request, "finished",
+                                G_CALLBACK(IterLookupHelper::finished_cb), (void*) this);
+    }
+};
+
+void IterLookupHelper::directoryExplored()
+{
+    kDebug() << "directory explored";
+    QString findEntry = m_remainingComponents.pop();
+    bool hasChildren = m_currentIter.child();
+    if ( ! hasChildren ) {
+        emit failed();
+        return;
+    }
+
+    bool found = false;
+    do {
+        kDebug() << m_currentIter.name();
+        if ( m_currentIter.name() == findEntry ) {
+            found = true;
+            break;
+        }
+    } while ( m_currentIter.next() );
+
+    if ( m_remainingComponents.isEmpty() ) {
+        // no directories remain
+        emit done(m_currentIter);
+        return;
+    }
+    else if ( found ) {
+        return explore(m_currentIter);
+    }
+    kWarning() << "explore failed!";
+    emit failed();
+};
---
+++ kobby/utils.h
@@ -0,0 +1,67 @@
+/* This file is part of the KDE libraries
+   Copyright (C) 2013 Sven Brauch <svenbrauch@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KOBBY_UTILS_H
+#define KOBBY_UTILS_H
+
+#include <QObject>
+#include <QStack>
+#include <KDebug>
+
+#include <glib.h>
+
+#include <libqinfinity/browser.h>
+
+// Helper class for finding the BrowserIter for a directory.
+// libinfinity works with documents (or directories) only as "iters",
+// which are basically iterators of a tree model which represents
+// the file structure. When asking about a document, e.g. when you want to
+// subscribe to it, you need to provide this iterator; the URL won't be accepted.
+// Since getting the contents of a directory
+// requires asking the server and waiting for a reply sometimes,
+// this class provides a convenient way to retrieve the iterator for
+// a given path.
+// Connect to the done() signal to get notified when the iter has been found.
+// Exploration results are cached by the underlying library, so this
+// operation is fast when it has been done before for the given path (excluding
+// the last entry), and network-slow if it has not (might need to display
+// a busy indicator while it's running).
+class IterLookupHelper : public QObject {
+Q_OBJECT
+public:
+    IterLookupHelper(QString lookupPath, QInfinity::Browser* browser);
+    static void finished_cb( InfcNodeRequest* request, void* user_data );
+
+    inline void begin() {
+        explore(m_currentIter);
+    };
+
+signals:
+    void done(QInfinity::BrowserIter found);
+    void failed();
+
+protected:
+    void directoryExplored();
+    void explore(QInfinity::BrowserIter directory);
+
+    QStack<QString> m_remainingComponents;
+    QInfinity::Browser* m_browser;
+    QInfinity::BrowserIter m_currentIter;
+};
+
+#endif
