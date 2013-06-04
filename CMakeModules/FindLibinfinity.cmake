04491a67669af1fa550f66ea25dcf468669dda05
David Edmundson <kde@davidedmundson.co.uk>
2013-06-04 23:41:17 +0100

    Update find libinfinity CMake modules

--- CMakeModules/FindLibinfinity.cmake
+++ CMakeModules/FindLibinfinity.cmake
@@ -2,7 +2,7 @@
 #
 # This script will define:
 #	LIBINFINITY_FOUND
-#	LIBINFINITY_INCLUDE_DIR
+#	LIBINFINITY_INCLUDES
 #	LIBINFINITY_LIBRARY
 #
 # Copyright (c) 2008 Gregory Haynes <greg@greghaynes.net>
@@ -11,57 +11,62 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
-if(LIBINFINITY_LIBRARIES AND LIBINFINITY_INCLUDES)
-	# Already found
-	set( LIBINFINITY_FIND_QUIETLY true )
-endif(LIBINFINITY_LIBRARIES AND LIBINFINITY_INCLUDES)
-
-find_package(Glib2 REQUIRED)
-find_package(GObject REQUIRED)
-find_package(GSASL REQUIRED)
-find_package(LibXml2 REQUIRED)
+if( LIBINFINITY_LIBRARIES AND LIBINFINITY_INCLUDES )
 	
-if(NOT WIN32)
-	include(UsePkgConfig)
-	pkgconfig(libinfinity-0.3
-		_LIBINFINITY_INCLUDE_DIR
-		_LIBINFINITY_LIBRARY_DIR
-		_LIBINFINITY_LINKER_FLAGS
-		_LIBINFINITY_COMPILER_FLAGS
+	set( LIBINFINITY_FOUND true )
+
+else( LIBINFINITY_LIBRARIES AND LIBINFINITY_INCLUDES )
+	
+	set( LIBINFINITY_FOUND false )
+
+	find_package(Glib2 REQUIRED)
+	find_package(GObject REQUIRED)
+	find_package(GSASL REQUIRED)
+	find_package(LibXml2 REQUIRED)
+	
+	if( ${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4 )
+		include(UsePkgConfig)
+		pkgconfig(libinfinity-0.5
+			_LIBINFINITY_INCLUDE_DIR
+			_LIBINFINITY_LIBRARY_DIR
+			_LIBINFINITY_LINKER_FLAGS
+			_LIBINFINITY_COMPILER_FLAGS
+		)
+	endif( ${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4 )
+	
+	find_path( LIBINFINITY_INCLUDES
+		NAMES
+			libinfinity/inf-config.h
+		PATHS
+			${_LIBINFINITY_INCLUDE_DIR}
+			/usr/include
+			/usr/local/include
+			/opt/local/include
+		PATH_SUFFIXES
+			libinfinity-0.5
 	)
-endif(NOT WIN32)
+	
+	find_library( LIBINFINITY_LIBRARY
+		NAMES
+			infinity-0.5
+		PATHS
+			${_LIBINFINITY_LIBRARY_DIR}
+			/usr/lib
+			/usr/local/lib
+			/usr/lib${LIB_SUFFIX}
+			/usr/local/lib${LIB_SUFFIX}
+	)
 
-find_path( LIBINFINITY_INCLUDES
-	NAMES
-		libinfinity/inf-config.h
-	PATHS
-		${_LIBINFINITY_INCLUDE_DIR}
-		/usr/include
-		/usr/local/include
-		/opt/local/include
-	PATH_SUFFIXES
-		libinfinity-0.4
-)
+	set( LIBINFINITY_INCLUDES
+		${LIBINFINITY_INCLUDES}
+		${GLIB2_INCLUDE_DIR}
+		${GOBJECT_INCLUDE_DIR}
+		${GSASL_INCLUDE_DIR}
+		${LIBXML2_INCLUDES} )
+	set( LIBINFINITY_LIBRARIES
+		${LIBINFINITY_LIBRARY}
+		${GOBJECT_LIBRARIES}
+		${GSASL_LIBRARIES}
+		${LIBXML2_LIBRARIES} )
 
-find_library( LIBINFINITY_LIBRARY
-	NAMES
-		infinity-0.4
-	PATHS
-		${_LIBINFINITY_LIBRARY_DIR}
-		/usr/lib
-		/usr/local/lib
-		/opt/local/lib
-)
-
-set( LIBINFINITY_INCLUDES
-	${LIBINFINITY_INCLUDES}
-	${GLIB2_INCLUDE_DIR}
-	${GOBJECT_INCLUDE_DIR}
-	${GSASL_INCLUDE_DIR}
-	${LIBXML2_INCLUDES} )
-set( LIBINFINITY_LIBRARIES
-	${LIBINFINITY_LIBRARY}
-	${GLIB2_LIBRARIES}
-	${GOBJECT_LIBRARIES}
-	${GSASL_LIBRARIES}
-	${LIBXML2_LIBRARIES} )
+endif( LIBINFINITY_LIBRARIES AND LIBINFINITY_INCLUDES )
