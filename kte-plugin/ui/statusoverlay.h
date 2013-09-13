37f0519fada1bab96aac657ebaac56195c3f523f
Sven Brauch <svenbrauch@googlemail.com>
2013-09-13 12:42:52 +0200

    Documentation for statusoverlay.h

--- kte-plugin/ui/statusoverlay.h
+++ kte-plugin/ui/statusoverlay.h
@@ -36,25 +36,52 @@ namespace KTextEditor {
 using Kobby::Document;
 using Kobby::Connection;
 
+/**
+ * @brief This class provides the overlay which is displayed when a document is loading.
+ */
 class StatusOverlay : public QDeclarativeView
 {
 Q_OBJECT
 public:
+    /**
+     * @brief Constructs a new overlay, but does not show it. Size of the given @p parent is tracked.
+     */
     explicit StatusOverlay(KTextEditor::View* parent);
+
+    /**
+     * @brief Event filter for tracking the parent's size
+     */
     virtual bool eventFilter(QObject* watched, QEvent* e);
+
+    /**
+     * @brief Display @p text in the text box over the progress bar.
+     */
     void displayText(const QString& text);
 
 public slots:
+    /**
+     * @brief Called when synchronization progresses. Updates the progress bar and text display.
+     * @param percentage Percentage completed, from 0.0 to 1.0
+     */
     void progress(double percentage);
     void loadStateChanged(Document*,Document::LoadState);
     void connectionStatusChanged(Connection*,QInfinity::XmlConnection::Status);
 
 private:
+    /**
+     * @brief Resizes the overlay to the parent view's size
+     */
     void resizeToView();
+    /**
+     * @brief Updates the displayed progressbar with a given value.
+     * @param percentage 0.0 = empty, 1.0 = full
+     */
     void setProgressBar(double percentage);
 
 private:
     KTextEditor::View* m_view;
+    /// Timer to prevent too frequent updates. On my machine, a redraw takes about
+    /// 5ms, so make sure we don't repaint more than 10 times a second in any case.
     QTime m_maxUpdateRateTimer;
 };
 
